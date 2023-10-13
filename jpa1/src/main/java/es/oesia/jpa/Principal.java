030ca910af807e0b15cc935304d19dc0cf953c57
cecilioAlvarez <cmac@arquitecturajava.com>
2023-10-13 10:06:11 +0200

    entitymanager

--- jpa1/src/main/java/es/oesia/jpa/Principal.java
+++ jpa1/src/main/java/es/oesia/jpa/Principal.java
@@ -2,11 +2,20 @@ package es.oesia.jpa;
 
 import java.util.Date;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
+		EntityManagerFactory emf= Persistence.createEntityManagerFactory("oesia");
+		EntityManager em= emf.createEntityManager();
+				
 		Libro libro= new Libro("1","Java","Pedro", new Date());
-		
+		em.getTransaction().begin();
+		em.persist(libro);
+		em.getTransaction().commit();
 	}
 }
